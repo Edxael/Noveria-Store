497507ea4f8f4df21895f1a3e0b450eedb0472e3
Edxael <zerphyr.r@gmail.com>
2017-09-23 22:26:20 -0600

    add update profile

--- app.js
+++ app.js
@@ -49,6 +49,11 @@ app.use(session({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res,next){
+  res.locals.user = req.user;
+  next();
+});
+
 
 app.use(mainRoutes);
 app.use(userRoutes);
